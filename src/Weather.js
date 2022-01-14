64bf12ad1875f81a2c478397c90c1ce9907028b0
Luciana B Ferrand <lbferrand@gmail.com>
2022-01-13 21:06:44 -0500

    Added api and loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,14 @@
 import React from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
-    alert(`The weather in New York is ${response.data.main.temp}`);
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
   }
 
-  let apiKey = "a0321581dcecd509e313e60aef62d42d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  //let apiKey = "a0321581dcecd509e313e60aef62d42d";
+  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return <Loader type="Hearts" color="#00BFFF" height={80} width={80} />;
 }
